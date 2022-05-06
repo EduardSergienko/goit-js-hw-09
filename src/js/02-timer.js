8c30b4b9ce2561bb3013cc9b46b8093370b19bac
EduardSergienko <katleraster@gmail.com>
2022-05-06 13:48:22 +0300

    Вывел в консоль таймер

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -16,13 +16,46 @@ const calendar = flatpickr('#datetime-picker', {
   minuteIncrement: 1,
 
   onClose(selectedDates) {
-    let currentTime = new Date().getTime();
+    let currentTime = Date.now();
     let selectedTime = selectedDates[0].getTime();
 
     if (currentTime > selectedTime) {
       window.alert('Please choose a date in the future');
+      return;
     } else {
       startBtn.removeAttribute('disabled');
     }
+    startBtn.addEventListener('click', onStartBtnCkick);
+    function onStartBtnCkick() {
+      setInterval(() => {
+        const curTime = Date.now();
+        const deltaTime = selectedTime - curTime;
+        const { days, hours, minutes, seconds } = convertMs(deltaTime);
+        console.log(`${days}:${hours}:${minutes}:${seconds}`);
+      }, 1000);
+    }
   },
 });
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
